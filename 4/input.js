3579f89e7d1c7581864bd14b8ea21a5c4131a8be
Sasho32 <sashosasho32@gmail.com>
2018-12-10 12:45:16 -0800

    regex

---
+++ 4/input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /[A-Z][a-z]+/,
+"exercise_2": /(088)[^0]\d{6}/,
+"exercise_3": /[^01]+/,
+"exercise_4": /^[a-z][a-zA-Z0-9_.]{2,30}$/,
+"exercise_5": /^(1500|1[0-4][0-9]{2}|[1-9][0-9][0-9]|[1-9][0-9]|[0-9])$/,
+"exercise_6": /class=['"].*['"]/
+};
+cssSelectors = {
+"exercise_1": "item > java.class1",
+"exercise_2": "different > java.diffClass",
+"exercise_3": "item > java.class1 > tag.class1 class2",
+"exercise_4": "css",
+"exercise_5": "css",
+"exercise_6": "css",
+"exercise_7": "css",
+"exercise_8": "css"
+};
